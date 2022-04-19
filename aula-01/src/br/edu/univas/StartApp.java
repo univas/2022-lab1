18da4d9cbb844c12ed3ff5be54b4821ad51f481d
Your Name <you@example.com>
2022-04-18 21:20:41 -0300

    Add aula-01

---
+++ aula-01/src/br/edu/univas/StartApp.java
@@ -0,0 +1,45 @@
+package br.edu.univas;
+
+public class StartApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+
+        int idade = 10;
+        System.out.println(idade);
+        System.out.println("idade");
+
+        String nome = "Rodrigo Luís de Faria";
+        System.out.println(nome);
+
+        float peso = 25.5f;
+        System.out.println(peso);
+
+        short a = 10;
+        byte b = 11;
+        int c = 12;
+        long d = 13;
+        float e = 14.4f;
+        double f = 15.5;
+        char g = 'R';
+        boolean h = true;
+
+        String faculdade = "Univas";
+
+        String nomeADefinir = "";
+        String cidade = null;
+        //comentário de uma linha
+        //int naoVaiDarCerto = null;
+
+        /*
+        comentario
+        com
+        mais
+        de
+        uma
+        linha
+         */
+        System.out.println(nomeADefinir);
+        System.out.println(cidade);
+    }
+}
